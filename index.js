a68aa09e92eb86e2b60fb76831208d94c35198c2
Timmylu414 <timothylu144@gmail.com>
2023-09-16 13:58:59 -0400

    added web scraping

--- index.js
+++ index.js
@@ -1,12 +1,51 @@
-import express from "express";
+/*import express from "express";
 import bodyParser from "body-parser";
+
 require("dotenv").config();
 
 import { Configuration, OpenAIApi } from "openai";
+*/
 
+const puppeteer = require('puppeteer');
+
+const cheerio = require('cheerio');
+const { load } = require('cheerio');
+
+/*
 const app = express();
-const port = 3000;
+const port = 3001;
+*/
 
+(async () => {
+  const browser = await puppeteer.launch({
+    defaultViewport: {
+      
+      width: 500,
+      height: 900,
+    }
+  });
+
+  const page = await browser.newPage();
+  await page.goto("https://en.wikipedia.org/wiki/Canada");
+
+  await page.screenshot({ path: "image.png"});
+
+const pageData = await page.evaluate(() => {
+  return {
+    html: document.documentElement.innerHTML,
+    width: document.documentElement.clientWidth,
+    height: document.documentElement.clientHeight,
+  }
+})
+
+const $ = load(pageData.html);
+
+console.log($.text());
+
+  await browser.close();
+})();
+
+/*
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
@@ -26,3 +65,5 @@ app.post("/submit", (req, res) => {
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
+
+*/
